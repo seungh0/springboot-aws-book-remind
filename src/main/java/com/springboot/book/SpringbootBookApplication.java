e0c466bfe57f38b5c1129ec16309d2a65f6d1167
seungh0 <will.seungho@gmail.com>
2020-10-25 05:02:36 +0900

    chapter2: 스프링 부트에서 테스트 코드를 작성하자.

--- src/main/java/com/springboot/book/SpringbootBookApplication.java
+++ src/main/java/com/springboot/book/SpringbootBookApplication.java
@@ -1,4 +1,4 @@
-package com.springboot.book.springbootbook;
+package com.springboot.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
