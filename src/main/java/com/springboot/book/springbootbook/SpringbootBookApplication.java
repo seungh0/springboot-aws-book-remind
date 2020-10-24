53b769e66c14a620f368b356390517fdfff80352
seungh0 <will.seungho@gmail.com>
2020-10-25 04:40:00 +0900

    Initial commit

---
+++ src/main/java/com/springboot/book/springbootbook/SpringbootBookApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.book.springbootbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBookApplication.class, args);
+	}
+
+}
